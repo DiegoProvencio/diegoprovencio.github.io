1b0fb13c45e00589336b2208c74eaad5e7817909
Fokuss <diegoprovenciomoreno@gmail.com>
2026-02-15 12:36:17 +0100

    Set site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://diegoprovencio.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
